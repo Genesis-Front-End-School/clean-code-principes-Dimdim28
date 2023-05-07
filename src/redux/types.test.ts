15b758113146cd1cb3c90f91036ab1e52237c064
Dimdim28 <wwwdimanes1@gmail.com>
2023-05-07 13:07:15 +0300

    test: changed tests for global types

--- src/redux/types.test.ts
+++ src/redux/types.test.ts
@@ -2,14 +2,51 @@ import { Status, CourseMeta, Course } from "./types";
 import { CoursesSliceState, FetchCourseResponse } from "./courses/types";
 import { Lesson, Lessons, LessonsSliceState, FetchLessonsParams } from "./lessons/types";
 
+const CourseMetaMock = {
+    slug: "slug",
+    skills: ["skill1", "skill2"],
+    fullCourseProductId: "666",
+    fullCourseProductFamily: "hell",
+    courseVideoPreview: {
+        link: "link",
+        duration: 777,
+        previewImageLink: "link",
+    }
+}
+
+const CourseMock = {
+    id: 'this is id',
+    title: 'title',
+    tags: ['tag1', 'tag2'],
+    status: 'launched',
+    description: "descr",
+    duration: 1000,
+    lessonsCount: 100,
+    containsLockedLessons: true,
+    previewImageLink: "link",
+    rating: 10,
+    meta: CourseMetaMock,
+};
+
+function courseMetaTestFunction(CourseMetaParam: CourseMeta) {
+    return CourseMetaParam;
+}
+
+function CourseTestFunction(CourseParam: Course) {
+    return CourseParam;
+}
+
 describe('file in the redux directory with all global types', () => {
     it('has all enums', () => {
         expect(Status).toBeDefined();
     })
 
     it('has all types', () => {
-        const CourseMeta2 = {} as CourseMeta;
-        expect(CourseMeta2).toBeDefined();
+        const CourseMetaResult: CourseMeta = courseMetaTestFunction(CourseMetaMock);
+        expect(CourseMetaResult).toBeDefined();
+
+        const CourseResult: Course = CourseTestFunction(CourseMock);
+        expect(CourseResult).toBeDefined();
 
         const Course2 = {} as Course;
         expect(Course2).toBeDefined();
