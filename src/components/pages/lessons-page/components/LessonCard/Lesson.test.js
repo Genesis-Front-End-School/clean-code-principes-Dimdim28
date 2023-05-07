c2247347636e411a2e0185ead0afc4be20a8f7ee
Dimdim28 <wwwdimanes1@gmail.com>
2023-05-07 15:27:08 +0300

    test: added tests to LessonCard and Lessons

--- src/components/pages/lessons-page/components/LessonCard/Lesson.test.js
+++ src/components/pages/lessons-page/components/LessonCard/Lesson.test.js
@@ -1,8 +1,9 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import '@testing-library/jest-dom';
 
 import LessonCard from './LessonCard';
+import { act } from 'react-dom/test-utils';
 
 describe('VideoCard component ', () => {
   it('renders correctly if this is current lesson', () => {
@@ -62,5 +63,63 @@ describe('VideoCard component ', () => {
 
     const element = document.querySelector('.wrapper');
     expect(element).toBeInTheDocument();
+  });
+
+  it('can set current wideo after click', () => {
+    const setCurrentVideo = jest.fn((x) => x);
+    render(
+      <LessonCard
+        id=""
+        title="first lesson"
+        duration={564}
+        order={5}
+        type=""
+        link="duyeryuier"
+        status="unlocked"
+        previewImageLink="vbnghgh"
+        currentVideo={4}
+        setCurrentVideo={setCurrentVideo}
+      />,
+    );
+    const card = screen.getByRole('card');
+    act(() => fireEvent.click(card));
+    expect(setCurrentVideo).toHaveBeenCalledWith(4)
+  });
+
+  it('should test what goes into the state', () => {
+    const setCurrentVideo = jest.fn((x) => x);
+    render( <LessonCard
+        id=""
+        title="first lesson"
+        duration={564}
+        order={5}
+        type=""
+        link="duyeryuier"
+        status="unlocked"
+        previewImageLink="vbnghgh"
+        currentVideo={4}
+        setCurrentVideo={setCurrentVideo}
+        hasError={true}
+      />);
+      const card = screen.getByAltText('errorLessonImage');
+      expect(card).toBeInTheDocument()
+  });
+
+  it('should test what goes into the state', () => {
+    const setCurrentVideo = jest.fn((x) => x);
+    render( <LessonCard
+        id="vccbvjgf"
+        title="uniquetitle"
+        duration={564}
+        order={5}
+        type=""
+        link="duyeryuier"
+        status="unlocked"
+        previewImageLink=""
+        currentVideo={4}
+        setCurrentVideo={setCurrentVideo}
+      />);
+      const card = screen.getByAltText('uniquetitle');
+      expect(card).toBeInTheDocument()
   });
 });
